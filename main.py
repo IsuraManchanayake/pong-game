4aea3230659df67d35b4090ea4229affdd79db60
isuramanchanayake <isura.14@cse.mrt.ac.lk>
2017-02-05 13:41:51 +0530

    compeleted tutorial

--- main.py
+++ main.py
@@ -11,19 +11,51 @@ from random import randint
 class PongGame(Widget):
 
     ball = ObjectProperty(None)
+    p1 = ObjectProperty(None)
+    p2 = ObjectProperty(None)
 
     def serve_ball(self):
         self.ball.center = self.center
-        self.ball.v = Vector(7, 0).rotate(randint(0, 360))
+        self.ball.v = Vector(7, 0)  # .rotate(randint(0, 360))
 
     def update(self, dt):
         self.ball.move()
+
+        self.p1.bounce_ball(self.ball)
+        self.p2.bounce_ball(self.ball)
 
         if self.ball.y < 0 or self.ball.top > self.height:
             self.ball.vy *= -1
 
         if self.ball.x < 0 or self.ball.right > self.width:
             self.ball.vx *= -1
+
+        if self.ball.x < self.x:
+            self.p2.score += 1
+            self.serve_ball()
+
+        if self.ball.x > self.width:
+            self.p1.score += 1
+            self.serve_ball()
+
+    def on_touch_move(self, touch):
+        if touch.x < self.width / 3:
+            self.p1.center_y = touch.y
+        if touch.x > self.width * 2 / 3:
+            self.p2.center_y = touch.y
+
+
+class PongPaddle(Widget):
+
+    score = NumericProperty(0)
+
+    def bounce_ball(self, ball):
+        if self.collide_widget(ball):
+            vx, vy = ball.v
+            offset = (ball.center_y - self.center_y) / (self.height / 2)
+            bounced = Vector(-1 * vx, vy)
+            vel = bounced * 1.1
+            ball.v = vel.x, vel.y + offset
 
 
 class PongBall(Widget):
